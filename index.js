756265a1fa65bf7dde1fedd8078ec103d9175234
Tejus Bharadwaj <tejusbharadwaj@gmail.com>
2021-05-10 16:44:08 +0530

    change name

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import { getAllAvailbleVaccine } from "./src/services/Cowin.js";
-import { getConfig } from "./src/utils.js";
+import { getAllAvailbleVaccine } from "./src/services/Slots.js";
 
-const slots = await getAllAvailbleVaccine()
-if (slots.length !== 0) {
-    console.log(slots);
-} else {
-    console.log("There are no available slots right now")
-}
+(async function great() {
+    const slots = await getAllAvailbleVaccine()
+    if (slots.length !== 0) {
+        console.log(slots);
+        // return (
+        //     <div className="">
+        //         {slots.map((slotItem) => (
+        //             <div>
+        //                 {slotItem.center.name} | Available: {slotItem.session.available_capacity} | <button onClick={() => bookSlot(slotItem)}>{slotItem.session.slots[0]}</button>
+        //                 <br />
+        //                 <br />
+        //             </div>
+        //         ))}
+        //     </div>
+        // );
+    } else {
+        console.log("There are no available slots right now")
+        // return (
+        //     <div className="">
+        //         There are no available slots right now
+        //     </div>
+        // );
+    }
+})();
